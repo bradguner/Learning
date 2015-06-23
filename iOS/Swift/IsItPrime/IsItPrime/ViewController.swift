1924777887926173d95ef334db5c37d889275f07
bradguner <brad.guner@gmail.com>
2015-06-22 21:42:51 -0400

    app to determine if input number is prime
    initial commit

---
+++ iOS/Swift/IsItPrime/IsItPrime/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  IsItPrime
+//
+//  Created by Brad Guner on 2015-06-22.
+//  Copyright (c) 2015 Brad Guner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var enteredNum: UITextField!
+    @IBOutlet weak var primeLabel: UILabel!
+    
+    @IBAction func primeButton(sender: AnyObject) {
+        var num = enteredNum.text.toInt()
+        var prime = false
+        var i:Int = 2
+        if (num != nil) {
+            while (!prime) {
+                if (num! % i == 0) {
+                    primeLabel.text = "\(num!) is not prime."
+                    break
+                }
+                if (i == num!) {
+                    primeLabel.text = "\(num!) is prime."
+                    prime = true
+                }
+                i++
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
